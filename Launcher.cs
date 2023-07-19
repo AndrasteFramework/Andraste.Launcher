019c7dc0e22d24406c1ea109fa1163d7c3a6c1a5
MeFisto94 <MeFisto94@users.noreply.github.com>
2023-07-20 01:11:41 +0200

    Added support for attaching to running processes. It will always be preferable to launch the application instead and it's currently unclear how well the payload handles being attached.

--- Launcher.cs
+++ Launcher.cs
@@ -27,16 +27,34 @@ namespace Launcher
                         Console.WriteLine(
                             "No argument passed that contains the path to the game! Drag the game exe onto of this file or create a shortcut!");
                         Console.WriteLine("The second argument can be used to override the local DLL file name");
-                    #if !DEBUG
+#if !DEBUG
                         Console.WriteLine("Press ANY key to exit");
                         Console.ReadKey();
-                    #endif
+#endif
                         break;
                     case 1:
-                        launcher.Launch(args[0], "Andraste.Payload.Generic.dll");
+                    {
+                        if (int.TryParse(args[0], out var pid))
+                        {
+                            launcher.Attach(pid, "Andraste.Payload.Generic.dll");
+                        }
+                        else
+                        {
+                            launcher.Launch(args[0], "Andraste.Payload.Generic.dll");
+                        }
+                    }
                         break;
                     case 2:
-                        launcher.Launch(args[0], args[1]);
+                    {
+                        if (int.TryParse(args[0], out var pid))
+                        {
+                            launcher.Attach(pid, args[1]);
+                        }
+                        else
+                        {
+                            launcher.Launch(args[0], args[1]);
+                        }
+                    }
                         break;
                     default: // at least 3 arguments
                         if (!args[2].Equals("--"))
@@ -56,12 +74,61 @@ namespace Launcher
                 Console.WriteLine(
                     "An Exception happened while launching the game:");
                 Console.WriteLine(ex.ToString());
-                #if !DEBUG
+#if !DEBUG
                 Console.WriteLine("Press ANY key to exit");
                 Console.ReadKey();
-                #endif
-
+#endif
             }
+        }
+        
+        public void Attach(int processId, string dllName)
+        {
+            var proc = Process.GetProcessById(processId);
+            if (proc == null)
+            {
+                throw new ArgumentException("Could not find a process with id " + processId, nameof(processId));
+            }
+
+            var exePath = proc.MainModule!.FileName;
+            
+            var actualDllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dllName);
+            if (!File.Exists(actualDllPath))
+            {
+                throw new FileNotFoundException("Could not find mod framework dll file", dllName);
+            }
+            
+            // Boot up Andraste
+            Initialize();
+            Console.WriteLine($"Attaching to {exePath} with pid {processId}, injecting {actualDllPath}");
+
+            if (!WriteModsJson())
+            {
+                return;
+            }
+
+            SetupBindingRedirect(exePath, dllName);
+            
+            try
+            {
+                AttachToApplication(proc, actualDllPath);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There was an error while injecting into target:");
+                Console.ResetColor();
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+            }
+
+            LogAndBlockUntilTerminated(proc);
+
+#if !DEBUG
+            Console.WriteLine("Process exited");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+#endif
         }
 
         public void Launch(string exePath, string dllName, string launchArgs = "")
@@ -70,7 +137,7 @@ namespace Launcher
             {
                 throw new FileNotFoundException("Could not find application executable file", exePath);
             }
-            
+
             var actualDllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dllName);
             if (!File.Exists(actualDllPath))
             {
@@ -87,28 +154,8 @@ namespace Launcher
                 return;
             }
 
-            // Unfortunately, .NET FX requires us to add the config file with the bindings redirect, otherwise it fails to load assemblies.
-            // This fails when you run the game multiple times with different .configs (or if the .config is locked by the file?), but that's a corner case.
-            // TODO: In theory we'd need to merge files, because here, dllName.config does not containing transitive rewrites that are part in Andraste.Shared.dll.config
-            var bindingRedirectFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dllName + ".config");
-            var bindingRedirectShared = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Andraste.Shared.dll.config");
-            if (File.Exists(bindingRedirectFile))
-            {
-                File.Copy(bindingRedirectFile, exePath + ".config", true);
-                // For some reason, debugging has shown that sometimes, it tries to resolve the .configs in the Launcher directory. Is that dependant on the app?
-                File.Copy(bindingRedirectFile, Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(exePath)) + ".config", true);
-                //File.Copy(bindingRedirectShared, Path.Combine(Path.GetDirectoryName(exePath)!, "Andraste.Shared.dll.config"), true);
-            }
-            else if (File.Exists(bindingRedirectShared))
-            {
-                Console.WriteLine("Warning: Framework does not have a specific binding redirect file. Trying Andraste.Shared");
-                File.Copy(bindingRedirectShared, exePath + ".config", true);
-            }
-            else
-            {
-                Console.WriteLine($"Warning: Could not find a binding redirect file at {bindingRedirectFile}. Try to have your IDE generate one.");
-            }
-            
+            SetupBindingRedirect(exePath, dllName);
+
             var online = false;
             var mutex = new Mutex(false, online ? "44938b8f" : "957e4cc3"); // TDU2 specific hack.
 
@@ -135,15 +182,28 @@ namespace Launcher
                 return;
             }
 
+            LogAndBlockUntilTerminated(proc);
+
+            #if !DEBUG
+            Console.WriteLine("Process exited");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            #endif
+        }
+
+        private static void LogAndBlockUntilTerminated(Process proc)
+        {
             Console.Title = $"Andraste Console Launcher - Attached to PID {proc.Id}";
 
             #region Logging
+
             var output = new FileLoggingHost(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.log"));
             var err = new FileLoggingHost(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "error.log"));
             output.LoggingEvent += (sender, args) => Console.WriteLine(args.Text);
             err.LoggingEvent += (sender, args) => Console.Error.WriteLine(args.Text);
             output.StartListening();
             err.StartListening();
+
             #endregion
 
             // Keep this thread (and thus the application) running
@@ -152,12 +212,33 @@ namespace Launcher
             // Dispose/Cleanup
             output.StopListening();
             err.StopListening();
+        }
 
-            #if !DEBUG
-            Console.WriteLine("Process exited");
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            #endif
+        private static void SetupBindingRedirect(string exePath, string dllName)
+        {
+            // Unfortunately, .NET FX requires us to add the config file with the bindings redirect, otherwise it fails to load assemblies.
+            // This fails when you run the game multiple times with different .configs (or if the .config is locked by the file?), but that's a corner case.
+            // TODO: In theory we'd need to merge files, because here, dllName.config does not containing transitive rewrites that are part in Andraste.Shared.dll.config
+            var bindingRedirectFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dllName + ".config");
+            var bindingRedirectShared = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Andraste.Shared.dll.config");
+            if (File.Exists(bindingRedirectFile))
+            {
+                File.Copy(bindingRedirectFile, exePath + ".config", true);
+                // For some reason, debugging has shown that sometimes, it tries to resolve the .configs in the Launcher directory. Is that dependant on the app?
+                File.Copy(bindingRedirectFile,
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(exePath)) + ".config", true);
+                //File.Copy(bindingRedirectShared, Path.Combine(Path.GetDirectoryName(exePath)!, "Andraste.Shared.dll.config"), true);
+            }
+            else if (File.Exists(bindingRedirectShared))
+            {
+                Console.WriteLine("Warning: Framework does not have a specific binding redirect file. Trying Andraste.Shared");
+                File.Copy(bindingRedirectShared, exePath + ".config", true);
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"Warning: Could not find a binding redirect file at {bindingRedirectFile}. Try to have your IDE generate one.");
+            }
         }
 
         private static bool WriteModsJson()
